bed7ae2adcce32dbb8b831e82e70a302fcfb9b56
edwin-fsk <chaukeel@gmail.com>
2023-07-31 11:37:02 -0700

    function that prints an integer using putchar

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,36 +1,18 @@
 #include "main.h"
 /**
- * print_number - prints a number
+ * print_number - prints an int
  * @n: Input number
  */
-
 void print_number(int n)
 {
-	long len, res, i, temp, expo;
-
-	res = n;
-	expo = len =  1;
-
-	if (res < 0)
+	if (n < 0)
 	{
-		res *= -1;
 		_putchar('-');
+		n = -n;
 	}
-
-	temp = res;
-	while (temp >= 10)
+	if (n / 10 != 0)
 	{
-		len++;
-		temp /= 10;
+		print_number(n / 10);
 	}
-
-	for (i = 1; i < len; i++)
-		expo *= 10;
-
-	while (expo > 1)
-	{
-		_putchar((res / expo) % 10 + '0');
-		expo /= 10;
-	}
-	_putchar(res % 10 + '0');
+	_putchar(n % 10 + '0');
 }
