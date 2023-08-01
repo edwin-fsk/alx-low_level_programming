cfc3a808c7168dc54aef75296c5fed8f835c0bd9
edwin-fsk <chaukeel@gmail.com>
2023-08-01 12:34:48 -0700

    betty fixes

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,13 +8,12 @@
 size_t print_listint(const listint_t *a)
 {
 const listint_t *temp = a;
- size_t numOfnodes;
+size_t numOfnodes;
 
- for (numOfnodes = 0; temp != NULL; numOfnodes++)
-	{
-		printf("%d\n", temp->n);
-		temp = temp->next; /*point to the next node*/
-	}
-
- return (numOfnodes);
+for (numOfnodes = 0; temp != NULL; numOfnodes++)
+{
+	printf("%d\n", temp->n);
+	temp = temp->next; /*point to the next node*/
+}
+return (numOfnodes);
 }
